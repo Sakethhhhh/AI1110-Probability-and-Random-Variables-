eb8f35096069168b1c53bbea154871317d5f64b3
Sakethhhhh <132201711+Sakethhhhh@users.noreply.github.com>
2023-05-16 22:50:26 +0530

    Add files via upload

--- assign1/codes/assign1test.py
+++ assign1/codes/assign1test.py
@@ -22,6 +22,7 @@ y = binom.pmf(k_values,n,p)
 simlen = 100000
 
 data_binom = binom.rvs(n,p,size=simlen) #Simulating the event of a die rolled 2 times and noting the number of sixes
-
-fav_count = np.count_nonzero(data_binom == 2)*2+np.count_nonzero(data_binom==1)  #Finding the number of sixes in all the rolls
+fav_count=0
+for i in range(n):#counting sixes
+    fav_count = fav_count + np.count_nonzero(data_binom == i)*i 
 print(fav_count/simlen)  #printing the average number of sixes
